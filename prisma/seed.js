6268a55278ab8e91d50a2e33ee358c45ab99e81b
Catherine Li <catherine.uk.20@gmail.com>
2021-08-09 14:52:11 +0100

    seeding for pet created

---
+++ prisma/seed.js
@@ -0,0 +1,74 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const faker = require("faker");
+
+const boolean = [true, false];
+
+async function seed() {
+  // Prisma create query to seed models in database
+  const petList = buildAnimalDatabase();
+  const pets = await prisma.pet.createMany({
+    data: petList,
+  });
+
+  console.log(pets);
+}
+
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
+
+module.exports = {
+  seed,
+};
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function buildAnimalDatabase() {
+  const animalBreedsGenerators = {
+    dog: faker.animal.dog,
+    cat: faker.animal.cat,
+    snake: faker.animal.snake,
+    horse: faker.animal.horse,
+    bird: faker.animal.bird,
+    rabbit: faker.animal.rabbit,
+  };
+
+  const animalTypes = Object.keys(animalBreedsGenerators);
+
+  const animals = [];
+
+  animalTypes.forEach((type) => {
+    const breedGenerator = animalBreedsGenerators[type];
+    const numberOfBreeds = getRandomInt(1, 5);
+
+    for (let i = 0; i < numberOfBreeds; i++) {
+      const breed = breedGenerator();
+
+      const numberOfAnimals = getRandomInt(1, 5);
+
+      for (let i = 0; i < numberOfAnimals; i++) {
+        const booelanIndex = getRandomInt(0, boolean.length - 1);
+
+        const newAnimal = {
+          name: faker.name.firstName(),
+          age: getRandomInt(1, 20),
+          type,
+          breed,
+          microchip: boolean[booelanIndex],
+        };
+
+        animals.push(newAnimal);
+      }
+    }
+  });
+
+  return animals;
+}
